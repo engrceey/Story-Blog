ae284132928b77c53c2f9091bc7ec5794d304efe
zurum ogbonda <ogbondachristian@gmail.com>
2021-06-06 15:48:54 +0100

    views completed

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const dotenv = require('dotenv')
 const morgan = require('morgan')
 const exphbs = require('express-handlebars')
 const passport = require('passport')
+const methodOverride = require('method-override')
 const connectDB = require('./config/db')
 const session = require('express-session')
 const mongoose = require('mongoose')
@@ -21,19 +22,34 @@ const app = express()
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 
+app.use(
+  methodOverride(function (req, res) {
+    if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+      // look in urlencoded POST bodies and delete it
+      let method = req.body._method
+      delete req.body._method
+      return method
+    }
+  })
+)
 
 if(process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'));
 }
 
-const {formatDate, stripTags, truncate, editIcon} = require('./helpers/hbs')
+const {formatDate,
+   stripTags, 
+   truncate, 
+   editIcon,
+    select} = require('./helpers/hbs')
 
 // Register `hbs.engine` with the Express app.
 app.engine('.hbs', exphbs({ helpers: 
   {formatDate, 
     stripTags, 
     truncate,
-    editIcon
+    editIcon,
+    select
   },
   defaultLayout: 'main', extname: '.hbs'}));
 app.set('view engine', '.hbs');
